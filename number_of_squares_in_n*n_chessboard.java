910a7a7279186849a43fd1513fd63ccd8e7b34e1
ANKIT CHAUDHARY <42476350+ANKITCHAUDHARY-15@users.noreply.github.com>
2019-01-20 03:03:49 +0630

    Create number_of_squares_in_n*n_chessboard.java
    https://practice.geeksforgeeks.org/problems/squares-in-nn-chessboard/0

---
+++ number_of_squares_in_n*n_chessboard.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    System.out.println((n*(n+1)*(2*n+1))/6);
+		}
+	}
+}
